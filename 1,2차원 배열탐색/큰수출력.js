742b0ad3bcb76cbb9773ab7079ea4a9d33d9b446
heykkyu <heykkyu@gmail.com>
2022-01-13 15:50:40 +0900

    1/13

--- 1,2차원 배열탐색/큰수출력.js
+++ 1,2차원 배열탐색/큰수출력.js
@@ -1,9 +1,20 @@
-function soloution(s) {
-  let answer = 0;
+function soloution(s, arr) {
+  let answer = [];
+
+  for (let i of arr) {
+    if (answer.length === 0) {
+      answer.push(i);
+    } else {
+      if (i > s) {
+        answer.push(i)
+      }
+    }
+
+  }
 
   
   return answer;
 }
 
 
-console.log(soloution());
+console.log(soloution(5, [7,3,9,5,6,12]));

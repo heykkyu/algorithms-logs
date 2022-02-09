803516dd62bb5e02d498c982d76f725a79123979
heykkyu <heykkyu@gmail.com>
2022-02-09 16:07:12 +0900

    등수 구하기

--- 1,2차원 배열탐색/등수구하기.js
+++ 1,2차원 배열탐색/등수구하기.js
@@ -1,10 +1,18 @@
-function soloution(n) {
-  let answer = 0;
+function soloution(n, s) {
+  let answer = [];
+  let copy = [...s]
+  const sort = copy.sort((a, b) => {
+    return b - a;
+  })
   
+  // console.log(s.indexOf(89) + 1)
   
-  
+  for (let value of s) {
+    answer.push(sort.indexOf(value) + 1);
+  }
+
   return answer;
 }
 
 
-console.log(soloution(10));
+console.log(soloution(5, [87,89,92,100,76, 100]));

d21d80f976d8ed93ec48dd02a97329b4aa5f870a
heykkyu <heykkyu@gmail.com>
2022-02-20 18:14:35 +0900

    need info before완전탐색'

---
+++ 완전탐색(브루트포스)/뒤집은소수.js
@@ -0,0 +1,17 @@
+function solution(N,S) {
+  let answer;
+
+  function checkPrimeNum(int) {
+    return;
+  }
+
+  for (let value of S) {
+    let reverse = Number(String(value).split("").reverse().join(""));
+    console.log(reverse)
+  }
+
+  return answer;
+}
+
+
+console.log(solution(7, [128,460,603,40,521,137,123]));
---
+++ 완전탐색(브루트포스)/자릿수의합.js
@@ -0,0 +1,8 @@
+function solution(N,S) {
+  let answer, max=Number.MIN_SAFE_INTEGER;
+
+  return answer;
+}
+
+
+console.log(solution(7, [128,460,603,40,521,137,123]));

28c6853485e9bc0fdc942b5acd8be9b1b078977d
heykkyu <heykkyu@gmail.com>
2022-02-20 17:55:33 +0900

    문자열 압축 cnt

--- 문자열 탐색/문자열압축.js
+++ 문자열 탐색/문자열압축.js
@@ -1,6 +1,19 @@
-
 function soloution(n) {
   let answer = "";
+  let cnt = 1;
+
+  for (let i=0; i<n.length; i++) {
+    if (n[i] === n[i+1]) {
+      cnt++;
+    } else {
+      answer += n[i];
+      if (cnt > 1) {
+        answer += cnt;
+      }
+      cnt = 1;
+    }
+  }
+
 
   return answer;
 }

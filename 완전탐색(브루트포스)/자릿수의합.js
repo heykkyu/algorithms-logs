17ee31de0dee77c0d9cc738d60301741c2940b1d
heykkyu <heykkyu@gmail.com>
2022-03-31 17:33:29 +0900

    완전탐색 - 자릿수의 합

--- 완전탐색(브루트포스)/자릿수의합.js
+++ 완전탐색(브루트포스)/자릿수의합.js
@@ -1,6 +1,21 @@
 function solution(N,S) {
   let answer, max=Number.MIN_SAFE_INTEGER;
 
+  for (let value of S) {
+    let st = String(value).split("");
+    let sum = st.reduce((acc, cur) => {
+      return Number(acc) + Number(cur)
+    }, 0)
+
+    if (sum > max) {
+      answer = value;
+      max = sum;
+    } else if (max === sum) {
+      answer = answer > value ? answer : value;
+    }
+    
+  }
+
   return answer;
 }
 

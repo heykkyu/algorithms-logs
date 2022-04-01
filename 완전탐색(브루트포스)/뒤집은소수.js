3f83643d69ae3af8c2cdbcc85999a818334f5022
heykkyu <heykkyu@gmail.com>
2022-04-01 15:27:14 +0900

    뒤집은 소수

--- 완전탐색(브루트포스)/뒤집은소수.js
+++ 완전탐색(브루트포스)/뒤집은소수.js
@@ -1,17 +1,30 @@
 function solution(N,S) {
-  let answer;
+  let answer = [];
 
-  function checkPrimeNum(int) {
-    return;
+  function isPrime (number) {
+    if (number === 1) return false;
+
+    for (let i =2; i <= parseInt(number/2); i++) {
+      if (number % i === 0) return false;
+    }
+
+    return true;
   }
 
   for (let value of S) {
-    let reverse = Number(String(value).split("").reverse().join(""));
-    console.log(reverse)
+    let temp = value.toString().split("").reverse().join("");
+    
+    temp = Number(temp);
+
+    console.log(temp)
+    if(isPrime(temp)) {
+      answer.push(temp);
+    }
+
   }
 
   return answer;
 }
 
 
-console.log(solution(7, [128,460,603,40,521,137,123]));
+console.log(solution(7, [32,55,62,20,250,370,200,30,100]));

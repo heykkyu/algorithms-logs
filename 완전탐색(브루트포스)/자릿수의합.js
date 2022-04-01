3833f988b942a7b82e87918b31862a44cecb2c11
heykkyu <heykkyu@gmail.com>
2022-04-01 14:35:29 +0900

    자리수의합 숫자로 푸는 경우 추가

--- 완전탐색(브루트포스)/자릿수의합.js
+++ 완전탐색(브루트포스)/자릿수의합.js
@@ -1,4 +1,4 @@
-function solution(N,S) {
+function stringSolution(N,S) {
   let answer, max=Number.MIN_SAFE_INTEGER;
 
   for (let value of S) {
@@ -19,5 +19,28 @@ function solution(N,S) {
   return answer;
 }
 
+function integerSolution(n, arr) {
+  let answer, max=Number.MIN_SAFE_INTEGER;
+  
+  for (let x of arr) {
+    let sum = 0, temp = x;
 
-console.log(solution(7, [128,460,603,40,521,137,123]));
+    while (temp > 0) {
+      sum += (temp%10);
+      temp = Math.floor(temp / 10);
+    }
+
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (max === sum && x > answer) {
+      answer = x;
+    }
+  }
+
+  return answer;
+}
+
+
+console.log(stringSolution(7, [128,460,603,40,521,137,123]));
+console.log(integerSolution(7, [128,460,603,40,521,137,123]));
